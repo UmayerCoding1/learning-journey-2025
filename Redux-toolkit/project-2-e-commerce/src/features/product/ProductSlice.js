01c737a47f2daaa6731ff41a2827d74db73cc9ef
umayerCoding1 <umayerpro175@gmail.com>
2025-07-04 10:09:40 +0600

    update product data and enhance product fetching with Redux integration

--- Redux-toolkit/project-2-e-commerce/src/features/product/ProductSlice.js
+++ Redux-toolkit/project-2-e-commerce/src/features/product/ProductSlice.js
@@ -1,16 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
 const initialState ={
     products: [],
     isLoaading: false,
     error: null
 }
 
+const BASE_URL = 'http://localhost:3003'
+
+ export const fetchProducts  = createAsyncThunk('products/fetchProducts',async() =>{
+   const res = await axios.get(`${BASE_URL}/products`);
+   return res.data
+});
+
+ export const deleteProducts  = createAsyncThunk('products/deleteProducts',async(id) =>{
+   const res = await axios.delete(`${BASE_URL}/${id}`);
+   return res.data
+});
 export const productSlice = createSlice({
    name: 'products',
    initialState,
    reducers: {
-    
+
+   },
+   extraReducers : (builder) => {
+    builder.addCase(fetchProducts.pending, (state) => {
+      state.isLoaading = true,
+      state.error = null
+    });
+    builder.addCase(fetchProducts.fulfilled, (state,actios) => {
+      state.isLoaading = false,
+      state.products = actios.payload
+      state.error = null
+    });
+    builder.addCase(fetchProducts.rejected, (state,actios) => {
+      state.isLoaading = false,
+      state.products = [];
+      state.error = actios.error.message || "Failed to fetch data";
+    });
    }
 });
 
--- Redux-toolkit/project-2-e-commerce/src/features/product/ProductsListView.jsx
+++ Redux-toolkit/project-2-e-commerce/src/features/product/ProductsListView.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { fetchProducts } from './ProductSlice';
 const ProductsListView = () => {
+    const {isLoaading,products,error} = useSelector((stste) => stste.productsR);
+    const dispatch = useDispatch();
+ console.log(products);
+ 
+    useEffect(() => {
+        dispatch(fetchProducts());
+    },[]);
+
+    if (isLoaading) {
+        return <p>Loading . . . </p>
+    }
     return (
         <div>
-            List of products
+            {isLoaading && <p>Loading . . . </p>}
+            {error && <p>{error}</p>}
+           <section className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-2'>
+             {!isLoaading && !error && products && products.length > 0 && products.map((product,inx) => {
+                return (
+                    <article className=' bg-gray-100 shadow-md border border-gray-200 rounded-lg p-2 transition-all ease-in-out duration-200 hover:bg-gray-200' key={inx}>
+                         <h2 className='text-2xl font-medium'>{product.title}</h2>
+                         <p>{product.description}</p>
+                    </article>
+                )
+            })}
+           </section>
         </div>
     );
 };
