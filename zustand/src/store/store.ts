f7b162f8d13694ded3b39231cbec5a2a116d7dec
umayerCoding1 <umayerpro175@gmail.com>
2025-10-19 00:02:09 +0600

    refactor: reorganize CounterStore type definition and improve code structure

--- zustand/src/store/store.ts
+++ zustand/src/store/store.ts
@@ -1,15 +1,22 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+type CounterStore = {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+  reset: () => void;
+};
 
-type CounterStore ={
-    count: number;
-    increment: () => void;
-    decrement: () => void;
-    reset: () => void;
-}
-
-export const useCountStore =  create<CounterStore>((set) => ({
+export const useCountStore = create<CounterStore>()(
+  persist((set) => ({
     count: 0,
     increment: () => set((state) => ({ count: state.count + 1 })),
     decrement: () => set((state) => ({ count: state.count - 1 })),
     reset: () => set({ count: 0 }),
-  }));
+  }),
+
+  {
+    name: "count-storage",
+  })
+);
+
