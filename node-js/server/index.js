1cbaded2640b3d6c10728dec4aa842a546d8491b
umayerCoding1 <umayerpro175@gmail.com>
2025-10-21 00:31:39 +0600

    feat: enhance HTTP server with URL parsing and dynamic response for about page

--- node-js/server/index.js
+++ node-js/server/index.js
@@ -1,17 +1,21 @@
 const http = require("http");
 const fs = require("fs");
 
+const url = require("url");
+
 const myServer = http.createServer((req, res) => {
 //   console.log(req);
   const log = `${req.method} ${req.url} ${req.httpVersion} ${req.headers.host}  IP: ${req.socket.remoteAddress}\n`;
-
+  const myUrl = url.parse(req.url , true);
+  console.log(myUrl)
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
         case '/' : 
         res.end('<h1>Home Page</h1>');
         break;
         case '/about' : 
-        res.end('<h1>About Page</h1>');
+        const username = myUrl.query.myname 
+        res.end(`<h1> Hi ${username}</h1>`);
         break;
         case '/contact' : 
         res.end('<h1>Contact Page</h1>');
